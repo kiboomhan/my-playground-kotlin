3846dd66cd40305748d582d1f5b98a016b5cdad8
kiboomhan <kiboomhan@gmail.com>
2022-10-17 12:07:25 +0900

    intellij bug : Unresolved reference: snippetsDir 간접 변수 선언 변경

--- src/main/kotlin/my/kkb/play/MyPlaygroundKotlinApplication.kt
+++ src/main/kotlin/my/kkb/play/MyPlaygroundKotlinApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class MyPlaygroundKotlinApplication
 
 fun main(args: Array<String>) {
-	runApplication<MyPlaygroundKotlinApplication>(*args)
+    runApplication<MyPlaygroundKotlinApplication>(*args)
 }
